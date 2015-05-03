9d24d79d37e6c61539ff82562165bb05a54df48d
Christopher Corley <cscorley@ua.edu>
2015-05-03 19:25:51 -0400

    Emit info about line/char position

--- src/srcmlolol/XMLListener.java
+++ src/srcmlolol/XMLListener.java
@@ -8,9 +8,9 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 public class XMLListener implements ParseTreeListener {
     private int depth = 0;
 
-    private void indent(){
+    private void indent() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < depth; i++){
+        for (int i = 0; i < depth; i++) {
             sb.append(" ");
         }
         System.out.print(sb.toString());
@@ -19,15 +19,39 @@ public class XMLListener implements ParseTreeListener {
     @Override
     public void enterEveryRule(ParserRuleContext ctx) {
         indent();
-        String name = ctx.getClass().getSimpleName().replace("Context", "");
-        System.out.println("<" + name + ">");
         depth++;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        sb.append(ctx.getClass().getSimpleName().replace("Context", ""));
+
+        sb.append(" start_line=");
+        sb.append(ctx.start.getLine());
+
+        sb.append(" end_line=");
+        sb.append(ctx.stop.getLine());
+
+        sb.append(" start_line_char=");
+        sb.append(ctx.start.getCharPositionInLine());
+
+        sb.append(" end_line_char=");
+        sb.append(ctx.stop.getCharPositionInLine() + ctx.stop.getText().length());
+
+        sb.append(" start_char=");
+        sb.append(ctx.start.getStartIndex());
+
+        sb.append(" end_char=");
+        sb.append(ctx.stop.getStopIndex());
+
+        sb.append(">");
+        System.out.println(sb.toString());
     }
 
     @Override
     public void exitEveryRule(ParserRuleContext ctx) {
         depth--;
         indent();
+
         String name = ctx.getClass().getSimpleName().replace("Context", "");
         System.out.println("</" + name + ">");
     }
@@ -40,10 +64,7 @@ public class XMLListener implements ParseTreeListener {
     public void visitTerminal(TerminalNode node) {
         indent();
         String text = node.getText();
-        if (text.equals("<EOF>")){
-            System.out.println("<EOF />");
-        }
-        else{
+        if (!text.equals("<EOF>")) {
             System.out.println(text);
         }
     }
